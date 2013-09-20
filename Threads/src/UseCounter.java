657055c837303acfb1c8170e9b511ea147899224
Rafael R. S. Robles <megaprog@gmail.com>
2013-09-20 15:19:38 -0430

    Testing threads (deadlocks)

--- Threads/src/UseCounter.java
+++ Threads/src/UseCounter.java
@@ -6,9 +6,9 @@
 // This class implements Runnable interface
 // Its run method increments the counter three times
 class UseCounter implements Runnable {
-    public void increment() {
-// increments the counter and prints the value
-// of the counter shared between threads
+    public synchronized void increment() {
+        // increments the counter and prints the value
+        // of the counter shared between threads
         Counter.count++;
         System.out.print(Counter.count + " ");
     }
